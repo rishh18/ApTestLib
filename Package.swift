c46f769634181120318ab200185fe31dccbe913b
rishh18 <rishabh.tripathi@adpushup.com>
2025-01-26 23:21:09 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     products: [
         .library(
             name: "ApTestLibrary",
-            targets: ["Sources/ApTestLib"]
+            targets: ["ApTestLibrary"]
         ),
     ],
     dependencies: [.package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git", from: "11.0.0")],

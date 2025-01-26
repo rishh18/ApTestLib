5885055b8e0ef15e227522dbe2abaf4283376c7d
rishh18 <rishabh.tripathi@adpushup.com>
2025-01-27 00:13:54 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -9,20 +9,20 @@ let package = Package(
     ],
     products: [
         .library(
-            name: "ApTestLibrary",
-            targets: ["ApTestLibrary"]
+            name: "ApTestLib",
+            targets: ["ApTestLib"]
         ),
     ],
     dependencies: [.package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git", from: "11.0.0")],
     targets: [
         .binaryTarget(
             name: "ApTestLibrary",
-            url: "https://github.com/rishh18/ApTestLib/releases/download/1.0.0/TestLibrary.xcframework.zip",
+            url: "https://github.com/rishh18/ApTestLibrary/releases/download/1.0.0/TestLibrary.xcframework.zip",
             checksum: "a8168b57232978ba2ecb9da13eb472f50f5e18466db372fc561fc388b3704ac5"
         ),
         .target(
           name: "ApTestLib",
-          dependencies: [
+          dependencies: [.target(name: "TestLibrary"),
                 .product(name: "GoogleMobileAds", package: "swift-package-manager-google-mobile-ads")
           ],
           path: "Sources/ApTestLib"

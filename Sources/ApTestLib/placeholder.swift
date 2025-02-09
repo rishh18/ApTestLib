04f062e47d63d39b1ddbd6b6134c33a2efb6e834
rishh18 <rishabh.tripathi@adpushup.com>
2025-02-10 02:19:09 +0530

    1.0.9 Release

--- Sources/ApTestLib/placeholder.swift
+++ Sources/ApTestLib/placeholder.swift
@@ -1,20 +1,20 @@
 // xcodebuild archive \
-//     -scheme "TestLibrary" \
-//     -destination "generic/platform=iOS" \
-//     -configuration Release \
-//     -archivePath "./build/iOS" \
-//     -sdk iphoneos \
-//     SKIP_INSTALL=NO \
-//     BUILD_LIBRARY_FOR_DISTRIBUTION=YES \
+// -workspace TestLibrary.xcworkspace \
+// -scheme TestLibrary \
+// -configuration Release \
+// -archivePath "./build/iOS" \
+// -sdk iphoneos \
+// SKIP_INSTALL=NO \
+// BUILD_LIBRARY_FOR_DISTRIBUTION=YES
 
 // xcodebuild archive \
-//     -scheme "TestLibrary" \
-//     -destination "generic/platform=iOS Simulator" \
-//     -configuration Release \
-//     -archivePath "./build/iOSSimulator" \
-//     -sdk iphonesimulator \
-//     SKIP_INSTALL=NO \
-//     BUILD_LIBRARY_FOR_DISTRIBUTION=YES \
+// -workspace TestLibrary.xcworkspace \
+// -scheme TestLibrary \
+// -configuration Release \
+// -archivePath "./build/iOSSimulator" \
+// -sdk iphonesimulator \
+// SKIP_INSTALL=NO \
+// BUILD_LIBRARY_FOR_DISTRIBUTION=YES
 
 // xcodebuild -create-xcframework \
 //     -framework "./build/iOS.xcarchive/Products/Library/Frameworks/TestLibrary.framework" \
